b7053e18f695e73087508a746dd5a09e17a54af2
ecit5 <esmoit5@gmail.com>
2022-03-11 15:40:54 +0300

    add layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return (  
+      
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+      
+      <div className='app-inner-container'>
+
+        <div className='cards-container'>
+              <div className='card'>
+                <div className='card-guest'>
+                  guest
+                </div>
+                <div className='card-topic'>
+                  topic
+                </div>
+                <button className='delete-card'>
+                
+                </button>          
+              </div>
+        </div>
+
+
+        <div className='tools-side'>
+
+
+          <div className='form'>
+            <input className='get-name'>
+
+            </input>
+            <input className='get-topic'>
+
+            </input>
+            <button className='add-card'>
+
+            </button>
+          </div>
+
+
+          <div className='history-container'>
+            <div className='history-label'>
+              history
+            </div>
+            <div className='cards-history-container'>
+            <div className='card-history'>
+                <div className='card-guest'>
+                  guesthistory
+                </div>
+                <div className='cardhistory-topic'>
+                  topichistory
+                </div>
+                <button className='delete-cardhistory'>
+                
+                </button>          
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+
+    </div>  
   );
 }
 
